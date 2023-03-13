a8357256bc637629b9731e1002bdc6af95c9a3ac
İbrahim Nakaş <127659228+ibrahimnakas@users.noreply.github.com>
2023-03-13 11:19:19 +0300

    Add files via upload

---
+++ hesapmakinesi/hesapmakinesi/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesapmakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double sayi1 = 0, sayi2 = 0;
+        char islem = ' ';
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text += "9";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text += "8";
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            islem = '+';
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text += "1";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text += "2";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text += "3";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text += "4";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text += "5";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text += "6";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text += "7";
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            islem = '-';
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            islem = '*';
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            islem = '/';
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            sayi1 = 0;
+            sayi2 = 0;
+            islem = ' ';
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            islem = '%';
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            sayi2 = Convert.ToDouble(textBox1.Text);
+            double sonuc = 0;
+            if (islem == '+')
+            {
+                sonuc = sayi1 + sayi2;
+            }
+            else if (islem == '-')
+            {
+                sonuc = sayi1 - sayi2;
+            }
+            else if (islem == '*')
+            {
+                sonuc = sayi1 * sayi2;
+            }
+            else if (islem == '/')
+            {
+                sonuc = sayi1 / sayi2;
+            }
+            else
+            {
+                sonuc = sayi1 % sayi2;
+            }
+            textBox1.Text = sonuc.ToString();
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0,";
+            }
+            else
+            {
+                textBox1.Text += ",";
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "0";
+            }
+
+        }
+    }
+}
